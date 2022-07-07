2fe2aa1a575aac6cef88ac96d1a33b93a867bd07
Yaroslav <jazaharov@gmail.com>
2022-07-07 21:55:59 +0300

    Builder

--- src/main/Aplication.java
+++ src/main/Aplication.java
@@ -1,4 +1,10 @@
 package main;
 
+import dish.builders.OrderBuilder;
+import dish.drink.Water;
+
 public class Aplication {
+    public static void main(String[] args) {
+        Order order = new OrderBuilder().addDrink(new Water()).build();
+    }
 }
